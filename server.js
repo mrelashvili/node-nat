ea4fcffd775b2a5000f938320005c3825524dd8f
Giorgi <mrmrela5@gmail.com>
2023-11-09 15:43:52 +0400

    connectiong database with express app & creating a simple tour model

--- server.js
+++ server.js
@@ -1,5 +1,42 @@
+const mongoose = require('mongoose');
+
 const dotenv = require('dotenv');
+
 dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
+  .then(con => {
+    console.log(`Database connected successfully`, con.connections);
+  });
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    uniqure: true
+  },
+  rating: {
+    type: Number,
+    default: 4.5
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price']
+  }
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
 const app = require('./app');
 
 app.listen(3000, () => {

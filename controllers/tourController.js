c0615c7a11d4059d58d3b8dcc3d060d6e373c065
Giorgi <mrmrela5@gmail.com>
2023-11-10 16:27:06 +0400

    reading documents from database

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -11,13 +11,19 @@ exports.checkBody = (req, res, next) => {
 };
 
 /// Route handlers
-exports.getAllTours = (req, res) => {
-  res.status(200).json({
-    status: 'success'
-    // data: {
-    //   tours
-    // }
-  });
+exports.getAllTours = async (req, res) => {
+  try {
+    const tours = await Tour.find();
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tours
+      }
+    });
+  } catch (err) {
+    res.status(404).json({ status: 'fail', message: err });
+  }
 };
 
 exports.createTour = async (req, res) => {
@@ -39,13 +45,22 @@ exports.createTour = async (req, res) => {
   // const newTour = new Tour({})
 };
 
-exports.getTour = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    data: {
-      // tours: tour
-    }
-  });
+exports.getTour = async (req, res) => {
+  try {
+    const tour = await Tour.findById(req.params.id);
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour
+      }
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err
+    });
+  }
 };
 
 exports.updateTour = (req, res) => {

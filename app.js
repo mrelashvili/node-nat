aad3bff9181563cd43a297418392932c09f18ea6
Giorgi <mrmrela5@gmail.com>
2023-11-13 18:48:11 +0400

    implement a global error handling middleware

--- app.js
+++ app.js
@@ -15,4 +15,32 @@ app.use(express.static(`${__dirname}/public`));
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
+/// Order matters. It needs to be last in middleware stack
+app.all('*', (req, res, next) => {
+  // res.status(404).json({
+  //   status: 'fail',
+  //   message: `Can't find ${req.originalUrl} on this server!`
+  // });
+  const err = new Error(`Can't find ${req.originalUrl} on this server!`);
+  err.status = 'fail';
+  err.statusCode = 404;
+
+  // if next receives argument it means it's error
+  next(err);
+});
+
+/// global error handling middleware
+/// When there's four parameter express identifies it as a error handling middleware
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message
+  });
+
+  next();
+});
+
 module.exports = app;
